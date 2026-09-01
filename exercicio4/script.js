69ec2a203b46a8afef782f1952a255c3e4c2fd0d
Sabrina Bloedow <sabrinabloedow80@gmail.com>
2026-09-01 00:31:30 -0300

    Add estrutura inicial

---
+++ exercicio4/script.js
@@ -0,0 +1,11 @@
+console.log("JavaScript funcionando!");
+
+function enviar() {
+    let nome = document.getElementById("nome").value;
+    const cpf = document.getElementById("cpf").value;
+    const dataNasc = document.getElementById("nascimento").value;
+
+    console.log(nome, cpf, dataNasc);
+    console.log("JavaScript funcionando!22");
+}
+
